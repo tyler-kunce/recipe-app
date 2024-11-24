1c10cb3b7ccb34afe7dc4058dc7dce256f0a204a
tyler-kunce <142066172+tyler-kunce@users.noreply.github.com>
2024-11-23 19:05:17 -0600

    Exercise 2.5

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -1,11 +1,42 @@
 from django.db import models
+from django.shortcuts import reverse
+
 
 # Create your models here.
 class Recipe(models.Model):
+    DIFFICULTY_CHOICES = [
+        ('Easy', 'Easy'),
+        ('Medium', 'Medium'),
+        ('Intermediate', 'Intermediate'),
+        ('Hard', 'Hard')
+    ]
+
     name = models.CharField(max_length=120)
     cooking_time = models.FloatField(help_text='(in minutes)')
-    ingredients = models.CharField(max_length=450)
-    difficulty = None
+    ingredients = models.TextField()
+    difficulty = models.CharField(max_length=50, choices=DIFFICULTY_CHOICES)
+    pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
 
     def __str__(self):
         return str(self.name)
+    
+    def get_absolute_url(self):
+        return reverse ('recipes:detail', kwargs={'pk': self.pk})
+    
+    def return_ingredients_as_list(self):
+        return self.ingredients.split(', ')
+        
+    def calculate_difficulty(self):
+        ingredient_len = len(self.return_ingredients_as_list())
+        if self.cooking_time < 10 and ingredient_len < 4:
+            return "Easy"
+        elif self.cooking_time < 10 and ingredient_len >= 4:
+            return "Medium"
+        elif self.cooking_time >= 10 and ingredient_len < 4:
+            return "Intermediate"
+        elif self.cooking_time >= 10 and ingredient_len >= 4:
+            return "Hard"
+    
+    def save(self, *args, **kwargs):
+        self.difficulty = self.calculate_difficulty()
+        super().save(*args, **kwargs)
--- src/recipes/tests.py
+++ src/recipes/tests.py
@@ -30,3 +30,9 @@ class RecipeModelTest(TestCase):
 
         # Compare the value to the expected result
         self.assertEqual(max_length, 120)
+
+    def test_get_absolute_url(self):
+        recipe = Recipe.objects.get(id=1)
+        # get_absolute_url() should take you to the detail page of recipe #1
+        # and load the URL /recipes/list/1
+        self.assertEqual(recipe.get_absolute_url(), '/recipes/list/1')
