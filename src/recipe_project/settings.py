acce3ed3c276824ed60ad4282021f2bec00c8e08
tyler-kunce <142066172+tyler-kunce@users.noreply.github.com>
2025-02-02 13:53:58 -0600

    Add Whitenoise for static files

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -129,10 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
