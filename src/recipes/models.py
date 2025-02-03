98fa1bdd92e62a7d8f1760f7dfb661936cb1c828
tyler-kunce <142066172+tyler-kunce@users.noreply.github.com>
2025-02-03 09:34:32 -0600

    Update models.py

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -15,7 +15,7 @@ class Recipe(models.Model):
     cooking_time = models.FloatField(help_text='(in minutes)')
     ingredients = models.TextField()
     difficulty = models.CharField(max_length=50, choices=DIFFICULTY_CHOICES)
-    pic = CloudinaryField('image')
+    pic = CloudinaryField('image', null=True, blank=True)
 
     def __str__(self):
         return str(self.name)
