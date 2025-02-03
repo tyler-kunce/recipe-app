3cf9df6c07f7fc085e9fbbfdc303d81a6d86d1ec
tyler-kunce <142066172+tyler-kunce@users.noreply.github.com>
2025-02-03 09:42:15 -0600

    Update admin.py

--- src/recipes/admin.py
+++ src/recipes/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from .models import Recipe
 
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'cooking_time', 'ingredients')
+    list_display = ('name', 'cooking_time', 'ingredients', 'image_preview')
     search_fields = ('name',)
-    readonly_fields = ('pic',)
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.pic:
+            return f'<img src="{obj.pic.url}" width="100" height="100" />'
+        return "No Image"
+    
+    image_preview.allow_tags = True
+    image_preview.short_description = "Preview"
 
 # Register your models here.
 admin.site.register(Recipe, RecipeAdmin)
