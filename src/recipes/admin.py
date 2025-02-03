8799e11be341a41a96cad15e9334dae855244e35
tyler-kunce <142066172+tyler-kunce@users.noreply.github.com>
2025-02-03 09:22:11 -0600

    Troubleshoot

--- src/recipes/admin.py
+++ src/recipes/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Recipe
 
+class RecipeAdmin(admin.ModelAdmin):
+    list_display('name', 'cooking_time', 'ingredients')
+    search_fields = ('name',)
+    readonly_fields = ('pic',)
+
 # Register your models here.
-admin.site.register(Recipe)
+admin.site.register(Recipe, RecipeAdmin)
